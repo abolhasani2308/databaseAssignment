d6f0b4298b7c8a18ff3e880f59dadb07a0c859be
abolhasani2308 <abolhasani2308@gmail.com>
2024-12-31 22:11:07 +0330

    add logger

--- src/main/java/com/databaseassignment/Main.java
+++ src/main/java/com/databaseassignment/Main.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.databaseassignment.entities.Category;
 import com.databaseassignment.entities.Product;
 import com.databaseassignment.entities.Customer;
@@ -11,7 +13,12 @@ import com.databaseassignment.entities.Orders;
 import com.databaseassignment.services.CustomerService;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final String GREEN = "\u001B[32m";
+    private static final String RESET = "\u001B[0m";
+
     public static void main(String[] args) {
+
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Category.class)
                 .addAnnotatedClass(Product.class)
@@ -20,7 +27,7 @@ public class Main {
                 .buildSessionFactory();
 
         Session session = factory.getCurrentSession();
-
+        
         try {
             session.beginTransaction();
 
@@ -29,14 +36,14 @@ public class Main {
             session.save(electronics);
 
             Product phone = new Product();
-            phone.setName("Smartphone");
-            phone.setPrice(699.99);
+            phone.setName("iphone 16");
+            phone.setPrice(90000000.0);
             phone.setCategory(electronics);
             session.save(phone);
 
             Customer customer = new Customer();
-            customer.setName("John Doe");
-            customer.setEmail("john.doe@example.com");
+            customer.setName("amir");
+            customer.setEmail("amir@gmail.com");
             session.save(customer);
 
             Orders orders = new Orders();
@@ -45,29 +52,30 @@ public class Main {
             session.save(orders);
 
             session.getTransaction().commit();
-            System.out.println("Data saved successfully!");
+            logger.info(GREEN + "Data saved successfully!" + RESET);
 
             session = factory.getCurrentSession();
             session.beginTransaction();
 
             String hql = "FROM Product";
             List<Product> products = session.createQuery(hql, Product.class).getResultList();
-            System.out.println("products size" + products.size());
+            logger.info(GREEN + "products size" + products.size() + RESET);
 
             for (Product product : products) {
-                System.out.println(product.getName() + ": " + product.getPrice());
+                logger.info(GREEN + product.getName() + ": " + product.getPrice() + RESET);
             }
 
             session.getTransaction().commit();
 
             CustomerService customerService = new CustomerService();
 
-            customerService.createCustomer("John Doe", "john@example.com");
+            customerService.createCustomer("Amir Mohammad", "amirmohammad@gmail.com");
     
             customerService.getAllCustomers().forEach(customerItem -> 
-                System.out.println(customerItem .getName() + " - " + customerItem .getEmail()));
+                logger.info(GREEN + customerItem.getName() + " " + customerItem.getEmail() + RESET)
+            );
     
-            customerService.updateCustomer(1L, "John Smith", "johnsmith@example.com");
+            customerService.updateCustomer(1L, "Amir Mohammad Abolhasani", "amirmohammad@gmail.com");
     
             customerService.deleteCustomer(1L);
     
